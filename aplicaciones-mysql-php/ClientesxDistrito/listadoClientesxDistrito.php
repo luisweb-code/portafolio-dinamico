d63e14467e9c01c604a5e20d6eb02dc968151bb8
Luis Sanchez <ingcomluis@gmail.com>
2020-09-03 16:19:08 -0500

    Subimos diagram de bases de datos ventas2015

---
+++ aplicaciones-mysql-php/ClientesxDistrito/listadoClientesxDistrito.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./">
+    <title>Clientes por Distrito</title>
+</head>
+<body>
+
+    <header>
+        <h2 class="centrado">lstados de clienets por distrito</h2>
+        <img src="" alt="imagen_portada">
+    </header>
+
+    <section>
+        <table border="1">
+            <thead>
+                <tr>CÓDIGO</tr>
+                <tr>CLIENTE</tr>
+                <tr>TELEFONO</tr>
+                <tr>DISTRITO</tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>c009</td>
+                    <td>manuel</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+
+    
+</body>
+</html>
