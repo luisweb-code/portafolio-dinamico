73296765d96933af7659c8b03f468e952904395e
Luis Sanchez <ingcomluis@gmail.com>
2021-01-28 17:33:47 -0600

    Avance de tienda en mvc php habra modificaciones

---
+++ webtienda/Config/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+const BASE_URL = "http://localhost/portafolio-dinamico/webtienda/";
+const LIBS = "Libraries/";
+const VIEWS = "Views/";
+
+
+
+
+
---
+++ webtienda/Models/homeModel.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+class homeModel{
+
+    public function __construct()
+    {
+        //echo "Mensaje desde el modelo HOME";
+    }
+
+   /*  public function getCarrito($params)
+    {
+        return "Datos del carrito No. ".$params;
+    } */
+
+
+}
+
+
