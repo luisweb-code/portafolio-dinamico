2ad754de0c22e1eedf3971f980502033302d9c0b
Luis Sanchez <ingcomluis@gmail.com>
2021-03-11 21:01:37 -0600

    terminacion de la maqueta falta contador de las candidatas

---
+++ aplicaciones-web-js/webcandidatas/js/main.js
@@ -0,0 +1,7 @@
+function getVoto() {
+  /* var botonVoto1 = document.getElementById("idVoto1"); */
+  var contador = 0;
+  let votoCandidata1 = contador++;
+  let setVoto = document.getElementById("idTotalVotos");
+  setVoto.innerText = votoCandidata1;
+}
