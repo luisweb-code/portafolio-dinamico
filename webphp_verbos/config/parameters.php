c52027f1faeda5501583d6dab015d6fef16888b9
Luis Sanchez <ingcomluis@gmail.com>
2021-04-23 20:00:07 -0500

    cambio de ruta en aplicacion verbos

--- webphp_verbos/config/parameters.php
+++ webphp_verbos/config/parameters.php
@@ -3,7 +3,7 @@
 //creamos las ruta y la constante de nuestro proyecto
 //constantes y sus valores
 
-define("base_url", "http://localhost/portafolio-dinamico/webverbo/");
+define("base_url", "http://localhost/portafolio-dinamico/webphp_verbos/");
 define("controller_default", "irregularController");
 define("action_default", "index");
 
