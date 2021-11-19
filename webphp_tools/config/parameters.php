ebc71e2d8809e06a1e332ea5078705feca67db13
Luis Sanchez <ingcomluis@gmail.com>
2021-11-18 23:34:31 -0600

    Eliminacion proyecto herramientas

--- webphp_tools/config/parameters.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-//creamos las ruta y la constante de nuestro proyecto
-//constantes y sus valores
-
-define("base_url", "http://localhost/portafolio-dinamico/webphp_review/");
-define("controller_default", "mainController");
-define("action_default", "index");
-
